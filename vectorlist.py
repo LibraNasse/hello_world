e7379c96ae4ac453f9387539433b3cf614867f73
LibraNasse <nazanin_afr@yahoo.com>
2016-07-18 17:37:58 +0200

    Add files via upload

---
+++ vectorlist.py
@@ -0,0 +1,29 @@
+class vectorList():
+    def __init__(self):
+        self.vector=[]
+        
+    def setVectorTo(self,vector):
+        self.vector=vector
+    def add(self,addVector):
+        return self.vector+addVector
+    def app(self,value):
+        return self.vector+[value]
+    def count(self, value):
+        counter=0
+        for i in self.vector:
+            if i==value:
+                counter +=1
+        return counter
+
+
+x=vectorList()
+x.setVectorTo([1,2,3,3])
+
+
+print x.app(7)
+
+print x.add([3,5,8])
+
+print x.app(7)
+
+print x.count(3)
