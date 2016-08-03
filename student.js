68b60074fd2f4a3b10783dc7b268a13ebe6289ee
LibraNasse <nazanin_afr@yahoo.com>
2016-08-03 21:47:38 +0200

    Add files via upload

---
+++ student.js
@@ -0,0 +1,21 @@
+class student{
+	constructor(public name: string){}
+	
+}
+
+class classroom{
+	constructer(public name: string)
+	
+}
+
+
+class subject{
+	constructer(public name: string)
+	
+}
+
+
+class group extends subject{
+	
+}
+
